8d0f1f0f8dac6810f3493523a314090ce6c54881
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-02 12:50:44 +0200

    Upgrade Skills table population

--- src/main/java/com/smartcarservice/ua/SmartCarService/initializer/LoadDatabase.java
+++ src/main/java/com/smartcarservice/ua/SmartCarService/initializer/LoadDatabase.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 import static java.lang.System.*;
 
 @Configuration
@@ -14,16 +16,32 @@ import static java.lang.System.*;
 public class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(SkillRepository repository){
+    CommandLineRunner initDatabase(SkillRepository repository) {
         return args -> {
-            repository.deleteAll();
-            out.println("Preloading " + repository.save(new Skill("Service Technician")));
-            out.println("Preloading " + repository.save(new Skill("Diagnostic Technician")));
-            out.println("Preloading " + repository.save(new Skill("Transmission Technician")));
-            out.println("Preloading " + repository.save(new Skill("Brake Technician")));
-            out.println("Preloading " + repository.save(new Skill("Body Repair Technician")));
-            out.println("Preloading " + repository.save(new Skill("Vehicle Refinisher")));
-            out.println("Preloading " + repository.save(new Skill("Vehicle Inspector")));
+            //repository.deleteAll();
+            List<Skill> list = repository.findAll();
+
+            if (!list.contains(new Skill("Service Technician"))) {
+                repository.save(new Skill("Service Technician"));
+            }
+            if (!list.contains(new Skill("Diagnostic Technician"))) {
+                repository.save(new Skill("Diagnostic Technician"));
+            }
+            if (!list.contains(new Skill("Transmission Technician"))) {
+                repository.save(new Skill("Transmission Technician"));
+            }
+            if (!list.contains(new Skill("Brake Technician"))) {
+                repository.save(new Skill("Brake Technician"));
+            }
+            if (!list.contains(new Skill("Body Repair Technician"))) {
+                repository.save(new Skill("Body Repair Technician"));
+            }
+            if (!list.contains(new Skill("Vehicle Refinisher"))) {
+                repository.save(new Skill("Vehicle Refinisher"));
+            }
+            if (!list.contains(new Skill("Vehicle Inspector"))) {
+                repository.save(new Skill("Vehicle Inspector"));
+            }
         };
     }
 }
