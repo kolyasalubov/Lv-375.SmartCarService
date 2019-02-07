8e93b69b5a4749cb364fcdbd0fd6ce79c7d3da34
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-07 17:59:29 +0200

    Added TechManagerProfile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { LineChartComponent } from 'src/charts/line-chart.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LineChartComponent } from 'src/charts/line-chart.component';
+import { TechmanagerProfileComponent } from './techmanager-profile/techmanager-profile.component';
+import { TechmanagerProfileService } from './techmanager-profile/techmanager-profile.service';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LineChartComponent
+    LineChartComponent,
+    TechmanagerProfileComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
+    FormsModule,
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [TechmanagerProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
