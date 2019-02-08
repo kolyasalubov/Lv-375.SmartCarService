b62a95c7dcae5b32b959e147b1a84001fd57cb7a
Severyn <tsyapkaseveryn@gmail.com>
2019-02-08 18:09:50 +0200

    Initial commit

---
+++ src/main/java/ua/ita/smartcarservice/SmartCarServiceApplication.java
@@ -0,0 +1,14 @@
+package ua.ita.smartcarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCarServiceApplication.class, args);
+	}
+
+}
+
