81f6b4ba56645ead15af154404c4c5c2d6cdcab7
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-08 09:03:18 +0200

    Turned Skill controller`s return value to ResponseEntity

--- src/main/java/ua/ita/smartcarservice/controller/SkillController.java
+++ src/main/java/ua/ita/smartcarservice/controller/SkillController.java
@@ -1,5 +1,7 @@
 package ua.ita.smartcarservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import ua.ita.smartcarservice.dto.stoDto.SkillDto;
 import ua.ita.smartcarservice.service.SkillService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class SkillController {
      * Method for getting all the skills
      */
     @GetMapping("/api/v1/skills")
-    public List<SkillDto> getAllSkill(){
-        return skillService.getAllSkills();
+    public ResponseEntity<List<SkillDto>> getAllSkill(){
+
+        ResponseEntity<List<SkillDto>> responseEntity;
+
+        try{
+            responseEntity = new ResponseEntity<>(skillService.getAllSkills(), HttpStatus.OK);
+        } catch (Exception e){
+            responseEntity = new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return responseEntity;
     }
 
 
