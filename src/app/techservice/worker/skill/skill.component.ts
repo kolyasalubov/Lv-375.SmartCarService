8d5437cb7ba192d17babba600d6f27c9322d4070
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-09 22:37:32 +0200

    Implemented service component

--- src/app/techservice/worker/skill/skill.component.ts
+++ src/app/techservice/worker/skill/skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from './skill';
+import { SkillService } from './skill.service';
+import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 @Component({
   selector: 'app-skill',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
-  constructor() { }
+  skills: Skill[];
+  error: ErrorEvent;
+
+  constructor(private skillService: SkillService) { }
 
   ngOnInit() {
+    console.log(this.skillService.getAllSkills()
+    .subscribe(data => this.skills = data,
+              error => this.error = error));
   }
-
 }
