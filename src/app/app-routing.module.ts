7faf033bb3f130794ecf29e8860e6db64faee448
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-10 22:28:35 +0200

    Technical Service Component implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
@@ -10,12 +10,27 @@ import { AdminComponent } from './admin/admin.component';
 
 import { ChartPageComponent } from './chart-page/chart-page.component';
 import { SkillComponent } from './techservice/worker/skill/skill.component';
+import { WorkerComponent } from './techservice/worker/worker.component';
+import { TechserviceComponent } from './techservice/techservice.component';
+import { TechmanagerProfileComponent } from './techmanager-profile/techmanager-profile.component';
 
 const routes: Routes = [
+    {
+        path: 'techmanager/profile',
+        component: TechmanagerProfileComponent
+    },
+    {
+        path: 'techservice',
+        component: TechserviceComponent
+    },
     {
         path: 'skills',
         component: SkillComponent
     },
+    {
+        path: 'workers',
+        component: WorkerComponent
+    }, 
     {
         path: 'home',
         component: HomeComponent
