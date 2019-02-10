7c76df8c8db84f64d1ccfb951ad491784c3cbdc2
ivannahavryliak <ivanna.havryliak@gmail.com>
2019-02-10 23:46:10 +0200

    UserEntity updated

--- src/main/java/ua/ita/smartcarservice/security/WebSecurityConfig.java
+++ src/main/java/ua/ita/smartcarservice/security/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.cors().and().csrf().disable()
+
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/api/skillBySto/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/skillByCar/**").permitAll()
