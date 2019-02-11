c1e8ca3d5eca39d8039c5b39af46d963b1f22085
Inesa09 <inesusja@gmail.com>
2019-02-11 23:58:34 +0200

    finish ChartService

--- src/app/chart-page/charts/chart/chart-dto.ts
+++ src/app/chart-page/charts/chart/chart-dto.ts
@@ -3,7 +3,7 @@ export class ChartDto {
     data: number[];
     labels: string[];
 
-    constructor(data: number[], labels: string[] ){
+    constructor(data: number[] = null, labels: string[] = null){
         this.data = data;
         this.labels = labels;
     }
