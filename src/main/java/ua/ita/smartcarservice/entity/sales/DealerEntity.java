294049afa3b4bafd7e97f34355a841bf501ee0b6
1 <ipanocko@gmail.com>
2019-02-16 14:40:30 +0200

    commit

--- src/main/java/ua/ita/smartcarservice/entity/sales/DealerEntity.java
+++ src/main/java/ua/ita/smartcarservice/entity/sales/DealerEntity.java
@@ -16,98 +16,95 @@ import java.util.List;
 
 public class DealerEntity {
 
-    public DealerEntity() {
+ 
+
+        public DealerEntity() {
+        }
+        @OneToOne
+        @JoinColumn(name = "user_id", nullable = false)
+        private UserEntity userEntity;
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long dealerId;
+
+        @Column(length = 100, nullable = false, unique = false)
+        private String dealerName;
+
+        @Column(length = 100, nullable = false, unique = false)
+        private String dealerAddress;
+
+        @Column(length = 100, nullable = false, unique = true)
+        private String dealerEdr;
+
+        @Column(length = 100, nullable = false, unique = true)
+        private String dealerEmail;
+
+
+        public DealerEntity(UserEntity userEntity, String dealerName, String dealerAddress, String dealerEdr, String dealerEmail) {
+            this.userEntity = userEntity;
+            this.dealerName = dealerName;
+            this.dealerAddress = dealerAddress;
+            this.dealerEdr = dealerEdr;
+            this.dealerEmail = dealerEmail;
+        }
+
+//    @OneToMany(mappedBy = "dealerEntity")
+//    private List<SaleManagerEntity>saleManagerEntities;
+//
+//
+//    @OneToMany(mappedBy = "dealerEntity")
+//    private List<TradeIn>tradeIns;
+//
+//    @OneToMany(mappedBy = "dealerEntity")
+//     private List<TechnicalServiceEntity>technicalServiceEntities;
+
+        public UserEntity getUserEntity() {
+            return userEntity;
+        }
+
+        public void setUserEntity(UserEntity userEntity) {
+            this.userEntity = userEntity;
+        }
+
+        public Long getDealerId() {
+            return dealerId;
+        }
+
+        public void setDealerId(Long dealerId) {
+            this.dealerId = dealerId;
+        }
+
+        public String getDealerName() {
+            return dealerName;
+        }
+
+        public void setDealerName(String dealerName) {
+            this.dealerName = dealerName;
+        }
+
+        public String getDealerAddress() {
+            return dealerAddress;
+        }
+
+        public void setDealerAddress(String dealerAddress) {
+            this.dealerAddress = dealerAddress;
+        }
+
+        public String getDealerEdr() {
+            return dealerEdr;
+        }
+
+        public void setDealerEdr(String dealerEdr) {
+            this.dealerEdr = dealerEdr;
+        }
+
+        public String getDealerEmail() {
+            return dealerEmail;
+        }
+
+        public void setDealerEmail(String dealerEmail) {
+            this.dealerEmail = dealerEmail;
+        }
+
     }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long dealerId;
-
-    @Column(length = 100, nullable = false, unique = false)
-    private String name;
-
-    @Column(length = 100, nullable = false, unique = false)
-    private String address;
-
-    @Column(length = 100, nullable = false, unique = true)
-    private String edr;
-
-    @OneToMany(mappedBy = "dealerEntity")
-    private List<UserDealer>userDealers;
-
-
-         @OneToMany(mappedBy = "dealerEntity")
-    private List<SaleManagerEntity>saleManagerEntities;
-
-
-    @OneToMany(mappedBy = "dealerEntity")
-    private List<TradeIn>tradeIns;
-
-    @OneToMany(mappedBy = "dealerEntity")
-     private List<TechnicalServiceEntity>technicalServiceEntities;
-
-
-    public Long getDealerId() {
-        return dealerId;
-    }
-
-    public void setDealerId(Long dealerId) {
-        this.dealerId = dealerId;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getEdr() {
-        return edr;
-    }
-
-    public void setEdr(String edr) {
-        this.edr = edr;
-    }
-
-    public List<UserDealer> getUserDealers() {
-        return userDealers;
-    }
-
-    public void setUserDealers(List<UserDealer> userDealers) {
-        this.userDealers = userDealers;
-    }
-
-    public List<SaleManagerEntity> getSaleManagerEntities() {
-        return saleManagerEntities;
-    }
-
-    public void setSaleManagerEntities(List<SaleManagerEntity> saleManagerEntities) {
-        this.saleManagerEntities = saleManagerEntities;
-    }
-
-    public List<TradeIn> getTradeIns() {
-        return tradeIns;
-    }
-
-    public void setTradeIns(List<TradeIn> tradeIns) {
-        this.tradeIns = tradeIns;
-    }
-
-    public List<TechnicalServiceEntity> getTechnicalServiceEntities() {
-        return technicalServiceEntities;
-    }
-
-    public void setTechnicalServiceEntities(List<TechnicalServiceEntity> technicalServiceEntities) {
-        this.technicalServiceEntities = technicalServiceEntities;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-}
