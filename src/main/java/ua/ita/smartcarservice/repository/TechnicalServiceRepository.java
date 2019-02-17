13dab389d44713c45a7f7637333d4b868d7d1611
cjhrxS <cjhrxS@gmail.com>
2019-02-17 12:15:24 +0200

    deleted useless code

--- src/main/java/ua/ita/smartcarservice/repository/TechnicalServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.ita.smartcarservice.repository;
-
-import ua.ita.smartcarservice.entity.sto.TechnicalManager;
-import ua.ita.smartcarservice.entity.sto.TechnicalService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechnicalServiceRepository extends JpaRepository<TechnicalService, Long> {
-    TechnicalService getByTechnicalManager(TechnicalManager technicalManager);
-}
--- src/main/java/ua/ita/smartcarservice/repository/sensors/SpeedRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.ita.smartcarservice.repository.sensors;
-
-import org.springframework.stereotype.Repository;
-import ua.ita.smartcarservice.entity.sensors.data.SpeedEntity;
-import ua.ita.smartcarservice.repository.sensors.factory.SensorRepository;
-
-@Repository
-public interface SpeedRepository extends SensorRepository<SpeedEntity> {
-}
