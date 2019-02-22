97fb8fddefaaadaba972cb64599d19f1a2758570
cjhrxS <cjhrxS@gmail.com>
2019-02-22 11:58:14 +0200

    small change

--- server/web/src/main/java/ua/ita/smartcarservice/App.java
+++ server/web/src/main/java/ua/ita/smartcarservice/App.java
@@ -2,9 +2,10 @@ package ua.ita.smartcarservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackageClasses = App.class)
+@SpringBootApplication
 @EnableScheduling
 public class App {
 	public static void main(String[] args) {
