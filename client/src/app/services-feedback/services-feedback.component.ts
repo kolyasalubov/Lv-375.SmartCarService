3450377458d432c13d88542c1bfc017d1623ddfb
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-02-25 17:40:42 +0200

    Added services-feedback and services-feedback-from and new logo file

---
+++ client/src/app/services-feedback/services-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-feedback',
+  templateUrl: './services-feedback.component.html',
+  styleUrls: ['./services-feedback.component.scss']
+})
+export class ServicesFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
