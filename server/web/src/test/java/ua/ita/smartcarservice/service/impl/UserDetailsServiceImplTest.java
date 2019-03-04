795b6bb05090c8e0fcd74e21547f49db651c4ee2
cjhrxS <cjhrxS@gmail.com>
2019-03-04 12:38:10 +0200

    added unit test for UserDetailServiceImpl

--- server/web/src/test/java/ua/ita/smartcarservice/service/impl/UserDetailsServiceImplTest.java
+++ server/web/src/test/java/ua/ita/smartcarservice/service/impl/UserDetailsServiceImplTest.java
@@ -1,15 +1,50 @@
 package ua.ita.smartcarservice.service.impl;
 
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockObjectFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.testng.Assert;
+import org.testng.IObjectFactory;
+import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
+import ua.ita.smartcarservice.entity.UserEntity;
+import ua.ita.smartcarservice.repository.UserRepository;
+import ua.ita.smartcarservice.service.UserPrinciple;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 
-
-
+@PrepareForTest(UserDetailsServiceImpl.class)
 public class UserDetailsServiceImplTest {
 
+    @ObjectFactory
+    public IObjectFactory setObjectFactory() {
+        return new PowerMockObjectFactory();
+    }
+
     @Test
-    public void testLoadUserByUsername() {
+    public void testLoadUserByUsername(String username) {
+
+        UserRepository userRepository = PowerMockito.mock(UserRepository.class);
+        UserPrinciple userPrinciple = PowerMockito.mock(UserPrinciple.class);
+        UserEntity userEntity = userRepository.findByUsername("admin").get();
+        PowerMockito.when(userRepository.findByUsername("admin")).thenReturn(java.util.Optional.of(userEntity));
+
+        Object actual;
+        Object expected;
+
+        expected = new UserEntity();
+        actual = userRepository.findByUsername("admin");
+
+        Assert.assertEquals(actual, expected);
+
+
     }
 }
