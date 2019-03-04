9423af9397ea2aa37758e6d0cb323d75786cd25f
Yaroslav Stanislavchuk <y380979684368@gmail.com>
2019-03-04 17:42:40 +0200

    Added test for TechnicalService Service

---
+++ server/web/src/test/java/ua/ita/smartcarservice/service/impl/technicalservice/TechnicalServiceServiceImplTest.java
@@ -0,0 +1,34 @@
+package ua.ita.smartcarservice.service.impl.technicalservice;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import ua.ita.smartcarservice.dto.technicalservice.TechnicalServiceDto;
+import ua.ita.smartcarservice.repository.technicalservice.TechnicalServiceRepository;
+import ua.ita.smartcarservice.service.technicalservice.TechnicalServiceService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@TestExecutionListeners(MockitoTestExecutionListener.class)
+@SpringBootTest
+public class TechnicalServiceServiceImplTest {
+
+    @MockBean
+    TechnicalServiceRepository technicalServiceRepository;
+
+    @Autowired
+    TechnicalServiceService technicalServiceService;
+
+    @Test
+    public void getTechnicalServiceDtoByUser() {
+        //Preconditions
+        List<TechnicalServiceDto> expectedDtoList = new ArrayList<>();
+        
+    }
+}
