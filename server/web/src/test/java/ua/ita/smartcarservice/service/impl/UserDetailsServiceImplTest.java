3309444e338ec840671150b5b65b4b35efae9533
cjhrxS <cjhrxS@gmail.com>
2019-03-04 11:42:26 +0200

    added powermock, mockito

---
+++ server/web/src/test/java/ua/ita/smartcarservice/service/impl/UserDetailsServiceImplTest.java
@@ -0,0 +1,15 @@
+package ua.ita.smartcarservice.service.impl;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+
+
+
+public class UserDetailsServiceImplTest {
+
+    @Test
+    public void testLoadUserByUsername() {
+    }
+}
