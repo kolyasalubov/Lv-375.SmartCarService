30b6c0aa6c955ae9d6991f7b74683e63149f45d8
cjhrxS <cjhrxS@gmail.com>
2019-03-04 18:55:02 +0200

    change persistance lvl

--- server/persistance/src/main/java/ua/ita/smartcarservice/repository/RoleRepository.java
+++ server/persistance/src/main/java/ua/ita/smartcarservice/repository/RoleRepository.java
@@ -2,18 +2,10 @@ package ua.ita.smartcarservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
-
 import ua.ita.smartcarservice.entity.RoleEntity;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-	
-	//Optional<RoleEntity> findByName(Roles roleName);
-	
-	RoleEntity findByName(String roleName);
-	
-	
 
+    RoleEntity findByName(String roleName);
 }
