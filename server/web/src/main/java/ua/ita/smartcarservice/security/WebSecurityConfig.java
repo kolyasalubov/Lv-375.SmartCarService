34642278ed81c87f6986ae6453a2423ec7a5c4be
Severyn <tsyapkaseveryn@gmail.com>
2019-03-15 20:43:26 +0200

    fix bug with web security

--- server/web/src/main/java/ua/ita/smartcarservice/security/WebSecurityConfig.java
+++ server/web/src/main/java/ua/ita/smartcarservice/security/WebSecurityConfig.java
@@ -61,10 +61,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/api/auth/**").permitAll()
-                .antMatchers("api/user/**", "/api/users/**", "/api/skills/**", "/api/workers/**", "/api/notifications/**").hasAuthority(Roles.ROLE_TECHNICAL_MANAGER.toString())
-                .antMatchers("/api/chart/**", "/api/record/**", "/api/report/**",  "api/workers/**", "api/works/**", "api/booking/**", "/api/notifications/**").hasAuthority(Roles.ROLE_CAR_OWNER.toString())
+                .antMatchers("api/user/**", "/api/users/**", "/api/skills/**", "/api/notifications/**").hasAuthority(Roles.ROLE_TECHNICAL_MANAGER.toString())
+                .antMatchers("/api/chart/**", "/api/record/**", "/api/report/**", "api/works/**", "api/booking/**", "/api/notifications/**").hasAuthority(Roles.ROLE_CAR_OWNER.toString())
                 .antMatchers("/api/owner/**", "/api/car/**", "/api/newcar/**").hasAnyAuthority(Roles.ROLE_CAR_OWNER.toString(), Roles.ROLE_DIELER.toString())
-                .antMatchers("/api/techservices/**").hasAnyAuthority(Roles.ROLE_TECHNICAL_MANAGER.toString(), Roles.ROLE_CAR_OWNER.toString())
+                .antMatchers("/api/techservices/**", "api/workers/**").hasAnyAuthority(Roles.ROLE_TECHNICAL_MANAGER.toString(), Roles.ROLE_CAR_OWNER.toString())
                 .antMatchers("/api/report/**").hasAuthority(Roles.ROLE_WORKER.toString())
                 .antMatchers("/api/dealer/**").hasAnyAuthority(Roles.ROLE_DIELER.toString(), Roles.ROLE_TECHNICAL_MANAGER.toString())
                 .anyRequest().permitAll();
